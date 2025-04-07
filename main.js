735b603cf581b8240e2079b24c4efd86b05cb1fe
D1EDlink <artemid.0918@gmail.com>
2025-04-07 18:15:46 +0200

    thebestupdatetoever1)

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Ссылки на элементы
+
 const links = {
     main: document.querySelector('.main-link'),
     about: document.querySelector('.about-link'),
@@ -7,42 +7,40 @@ const links = {
     comments: document.querySelector('.comments-link')
 };
 
-// Переменная для хранения текущей активной кнопки
+
 let activeBtn = document.querySelector('.active');
 
-// Обработчик клика на меню
+
 document.querySelector(".fixed-menu").addEventListener('click', function(event) {
     let clickTarget = event.target;
 
     if (clickTarget.classList.contains('nav-link')) {
-        // Если нажата другая кнопка, переключаем активный класс
         if (clickTarget !== activeBtn) {
             activeBtn.classList.remove('active');
             clickTarget.classList.add('active');
-            activeBtn = clickTarget; // Обновляем ссылку на активную кнопку
-        }
+            activeBtn = clickTarget;         }
     }
 });
 
-// Обработчик события прокрутки
+
 window.onscroll = function() {
     const h = document.documentElement.clientHeight;
     const scrollY = window.scrollY;
 
-    // Определяем, какую ссылку нужно активировать
+    
     let classLink = 'main';
     if (scrollY >= h * 4) classLink = 'comments';
     else if (scrollY >= h * 3) classLink = 'works';
     else if (scrollY >= h * 2) classLink = 'skills';
     else if (scrollY >= h) classLink = 'about';
 
-    // Получаем новую активную ссылку
+
     const newActiveBtn = links[classLink];
 
-    // Обновляем активный класс, если это необходимо
+    
     if (activeBtn !== newActiveBtn) {
         activeBtn.classList.remove('active');
         newActiveBtn.classList.add('active');
-        activeBtn = newActiveBtn; // Обновляем ссылку на активную кнопку
+        activeBtn = newActiveBtn; 
     }
 };
